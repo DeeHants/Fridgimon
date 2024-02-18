e79d313477a1968b941869f2d5f4fc5bac054255
Deanna Earley <dee@earlsoft.co.uk>
2024-02-18 11:36:00 +0000

    Add the initial webapp code

---
+++ src/webapp/eb/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Fridgimon scanner</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script type="text/javascript" charset="utf-8" src="ebapi-modules.js"></script>
+    </head>
+
+    <body>
+        <h1>Fridgimon</h1>
+    </body>
+</html>
