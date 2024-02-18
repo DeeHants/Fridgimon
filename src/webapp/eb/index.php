de0a7845519d1d0794dccdefd93500598c3a16bb
Deanna Earley <dee@earlsoft.co.uk>
2024-02-18 14:55:12 +0000

    Add handling for the button bar

--- src/webapp/eb/index.php
+++ src/webapp/eb/index.php
@@ -2,10 +2,30 @@
     <head>
         <title>Fridgimon scanner</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <META HTTP-Equiv="ButtonBar1" content="visibility:visible">
         <script type="text/javascript" charset="utf-8" src="ebapi-modules.js"></script>
+        <script type="text/javascript" charset="utf-8" src="elements.js"></script>
+        <script type="text/javascript">
+            document.addItem = function() {
+                alert("adding");
+            }
+
+            document.checkItem = function() {
+                alert("checking");
+            }
+
+            document.listItems = function() {
+                alert("listing");
+            }
+
+            document.removeItem = function() {
+                alert("removing");
+            }
+        </script>
     </head>
 
     <body>
+        <br/><br/>
         <h1>Fridgimon</h1>
     </body>
 </html>
