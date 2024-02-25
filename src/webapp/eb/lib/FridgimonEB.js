737f09baf7d75b73e38aa652aa46b903e8cf69c9
Deanna Earley <dee@earlsoft.co.uk>
2024-02-25 16:00:43 +0000

    Add initial react app

---
+++ src/webapp/eb/lib/FridgimonEB.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// export default
+function FridgimonEB() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Fridgimon"), /*#__PURE__*/React.createElement("div", {
+    id: "display"
+  }));
+}
---
+++ src/webapp/eb/src/FridgimonEB.jsx
@@ -0,0 +1,9 @@
+// export default
+function FridgimonEB() {
+    return (
+        <div>
+            <h1>Fridgimon</h1>
+            <div id="display"></div>
+        </div>
+    );
+}
