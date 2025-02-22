c5a9d55d08d29897cf7f212a17f68966a2ad59ab
Deanna Earley <dee@earlsoft.co.uk>
2025-02-22 23:48:37 +0000

    Correct the order for "unknown item" state variables

--- src/webapp/src/UnknownItem.jsx
+++ src/webapp/src/UnknownItem.jsx
@@ -1,10 +1,10 @@
 function UnknownItem({ item, onRefresh }) {
     // New item
     const [itemName, setItemName] = React.useState("");
-    const [itemVariant, setItemVariant] = React.useState(null);
     const [itemQualification, setItemQualification] = React.useState(null);
     const [itemQuantity, setItemQuantity] = React.useState(null);
     const [itemSize, setItemSize] = React.useState(null);
+    const [itemVariant, setItemVariant] = React.useState('default');
     const [itemCategory, setItemCategory] = React.useState(null);
     const [itemLife, setItemLife] = React.useState(null);
 
